3e634f884346c1ae945f4ad6d76442190238ccd1
Samuel Santos <samuel.lima22@aluno.cps.sp.gov.br>
2026-08-31 21:52:13 -0300

    Eu consegui fazer math

--- contas.php
+++ contas.php
@@ -8,17 +8,23 @@
 <body>
     
     <form action="matematica.php" method="get">
-    <pre>
-        <label for="">digite o primeiro numero:</label>
+        <pre>
+         <label for="">digite o primeiro numero:</label>
             <input type="number" name="numero1" id="">
+
+        <label for="">escolha a operacao:</label>
+            <select name="opcao">
+                <option value="1">Somar</option>
+                <option value="2">Subtrair</option>
+                <option value="3">Dividir</option>
+                <option value="4">Multiplicar</option>
+            </select>
+
             <label for="">digite o segundo numero:</label>
             <input type="number" name="numero2" id="">
 
             <input type="submit" value="enviar">    <input type="reset" value="limpar">
-
-
-
-    </pre>
+        </pre>
     </form>
 </body>
 </html>
--- matematica.php
+++ matematica.php
@@ -1,6 +1,15 @@
 <?php
 $numero1 = $_GET["numero1"];
 $numero2 = $_GET["numero2"];
-$soma = $numero1 + $numero2;
-echo $soma;
+$opcao = $_GET["opcao"];
+switch ($opcao) {
+case 1: $resultado = $numero1 + $numero2;
+break;
+case 2: $resultado = $numero1 - $numero2;
+break;
+case 3: $resultado = $numero1 / $numero2;
+break;
+case 4: $resultado = $numero1 * $numero2;
+break;}
+echo $resultado;
 ?>
