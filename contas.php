05565d90a7620d8723c2b0a96996e4435d6b0ecc
Samuel Santos <samuel.lima22@aluno.cps.sp.gov.br>
2026-08-31 21:21:05 -0300

    contas matematicas

---
+++ contas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="matematica.php" method="get">
+    <pre>
+        <label for="">digite o primeiro numero:</label>
+            <input type="number" name="numero1" id="">
+            <label for="">digite o segundo numero:</label>
+            <input type="number" name="numero2" id="">
+
+            <input type="submit" value="enviar">    <input type="reset" value="limpar">
+
+
+
+    </pre>
+    </form>
+</body>
+</html>
---
+++ matematica.php
@@ -0,0 +1,6 @@
+<?php
+$numero1 = $_GET["numero1"];
+$numero2 = $_GET["numero2"];
+$soma = $numero1 + $numero2;
+echo $soma;
+?>
